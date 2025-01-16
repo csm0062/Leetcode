8c643c5559ffd4f9912d84881d90b11e32f0291b
Choisoom <167949377+csm0062@users.noreply.github.com>
2025-01-17 04:48:18 +0900

    Time: 3 ms (42.15%), Space: 41.6 MB (77.79%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,43 +1,31 @@
 class Solution {
     public boolean isValid(String s) {
-        // 열린거 stack에 닫힌건 비교해서 빼기.
-        // 스택은 후입 선출
-        // 홀수 거르기
-        if(s.length() % 2 != 0)
+        if(s.length() % 2 != 0) {
             return false;
+        }
 
-        Stack<Character> stack = new Stack<Character>();
+        Stack<Character> stack = new Stack<>();
 
-        for (int i = 0; i < s.length(); i++) {
-            switch(s.charAt(i)) {
-                case ')':
-                    if(!stack.isEmpty() && stack.peek() == '(')
-                        stack.pop();
-                    else
-                        return false;
-                    break;
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
 
-                case '}':
-                    if(!stack.isEmpty() && stack.peek() == '{')
-                        stack.pop();
-                    else
-                        return false;
-                    break;
+            if(c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
 
-                case ']':
-                    if(!stack.isEmpty() && stack.peek() == '[')
-                        stack.pop();
-                    else
-                        return false;
-                    break;
-
-                // 열린괄호는 스택에 넣는다.    
-                default:
-                    stack.push(s.charAt(i));
-                    break;
+                char top = stack.peek();
+                if((c == ')' && top == '(') ||
+                (c == '}' && top == '{') ||
+                ( c == ']' && top == '[')) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
             }
         }
-        // 스택비면 true 반환
         return stack.isEmpty();
     }
 }
