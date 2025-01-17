1354e2005f03b3f877fb19eb01ee3d6229f94f3c
Choisoom <167949377+csm0062@users.noreply.github.com>
2025-01-18 03:10:47 +0900

    Time: 2 ms (98.94%), Space: 45.5 MB (8.68%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,14 +1,20 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        // {현재숫자(키): 인덱스(값)}-> 인덱스가 필요하니까
+        // 처음엔 {} 빈 값 이겠죠?
+        // temp가 해시맵에있는지 확인
         for(int i = 0; i < nums.length; i++) {
-            for(int j = i + 1; j < nums.length; j++) {
-                if(nums[i] + nums[j] == target) {
-                    return new int[]{i,j};
-                }
+            int temp = target - nums[i];// 값
+
+            // 만약 temp(해당키)가 있을 경우
+            if(map.containsKey(temp)) {
+                // {temp가 있는 인덱스, i} (새)배열 뽑기
+                return new int[] {map.get(temp), i};
             }
+            //해시맵에 entry추가
+            map.put(nums[i], i);
         }
-        return null; // 2개의 숫자가 target 값을 만들 지 못할 경우 처리
+        return new int[0];
     }
 }
-
-// 모든 경우의 수로 검색했기 때문에 비 효율적
