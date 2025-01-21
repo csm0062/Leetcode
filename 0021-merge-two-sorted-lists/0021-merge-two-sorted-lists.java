6b0b05a272fd3deeaabf42720dfeb945cebc31d5
Choisoom <167949377+csm0062@users.noreply.github.com>
2025-01-22 02:52:58 +0900

    Time: 0 ms (100%), Space: 42.8 MB (23.61%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -26,7 +26,7 @@ class Solution {
 
         if(list1 != null) {
             current.next = list1;
-        } else if (list2 != null) {
+        } else {
             current.next = list2;
         }
 
