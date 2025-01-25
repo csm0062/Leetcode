7a8707c0b907268a9d7bd77d7eed389125f22239
Choisoom <167949377+csm0062@users.noreply.github.com>
2025-01-26 01:53:29 +0900

    Time: 0 ms (100%), Space: 42 MB (15.71%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -16,9 +16,10 @@
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> output = new ArrayList<>(); // 결과값 나올 리스트만들기
-            inorder(root, output);
-            return output;
+        inorder(root, output);
+        return output;
     }
+    
     private void inorder(TreeNode node, List<Integer> output) {
         if (node != null) {
             inorder(node.left, output); // 왼쪽자식 노드 방문
